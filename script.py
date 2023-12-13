16991e219183ea8a4c110c77711a963c0b644e87
Tiger2lala <Msolo.tzhang@gmail.com>
2023-12-13 22:03:10 +0000

    Add histogram

--- script.py
+++ script.py
@@ -43,3 +43,40 @@ input_data = data_df.drop(variables_to_omit, axis = 1)
 r, c = input_data.shape
 print(f'Sample size                    : {r}')
 print(f'Number of independent variables: {c}')
+
+Malignant = data_df[data_df['diagnosis'] == 1]
+Benign = data_df[data_df['diagnosis'] == 0]
+worst_mean_se = ['area_worst', 'fractal_dimension_mean', 'radius_se']
+
+
+def makeHistogram(features):
+    for feature in features:
+        if not type(feature) is str:
+            raise TypeError('Only strings are permitted')
+
+    fig = plt.figure(figsize=(10, 8))
+    for i, feature in enumerate(features):
+        ax = fig.add_subplot(1, 3, i + 1)
+        sns.histplot(Malignant[feature],
+                     bins=bins,
+                     color='red',
+                     label='Malignant',
+                     kde=True)
+        sns.histplot(Benign[feature],
+                     bins=bins,
+                     color='green',
+                     label='Benign',
+                     kde=True)
+        plt.title(str(' Distribution of  ') + str(feature.replace('_', ' ').capitalize()))
+        plt.xlabel(str(feature.replace('_', ' ').capitalize()))
+        plt.ylabel('Density function')
+        plt.legend(loc='upper right')
+        ax.grid(False)
+
+    plt.tight_layout()
+    plt.show()
+
+
+bins = 'fd'  # Freedman and Diaconis
+
+makeHistogram(worst_mean_se)
