7449f4df14281b4361c00c9529e4ee540dc754a9
iris-ywang <iris_ywang@outlook.com>
2023-12-14 10:28:10 +0000

    added t test

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import seaborn as sns
+from scipy import stats # Hypthesis testing 
 
 data_df = pd.read_csv("datasets/data.csv")
 
@@ -43,3 +44,10 @@ input_data = data_df.drop(variables_to_omit, axis = 1)
 r, c = input_data.shape
 print(f'Sample size                    : {r}')
 print(f'Number of independent variables: {c}')
+
+# Make a new dataframe with only the desired feature for t test  
+hypothesis_test_data = pd.DataFrame(data = data_df[['area_worst', 'diagnosis']])
+hypothesis_test_data = hypothesis_test_data.set_index(Diagnosis)
+t, p = stats.ttest_ind(hypothesis_test_data.loc[0], hypothesis_test_data.loc[1])
+print(f'The t-value: {t}')
+print(f'The p-value: {p}')
